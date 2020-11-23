f83ef74303b7b49321faa65d031c404de078ed27
苏园 <ioodolab@gmail.com>
2020-11-23 16:18:08 +0800

    修改登录接口

--- app/libraries/RainLoop/Providers/ApiService/ApiServiceInterface.php
+++ app/libraries/RainLoop/Providers/ApiService/ApiServiceInterface.php
@@ -5,7 +5,7 @@ namespace RainLoop\Providers\ApiService;
 interface ApiServiceInterface
 {
 
-	// public function login($sUserCd, $sPassword);
+	public function login($sUserCd, $sPassword);
 	public function register($sUserCd, $sUserTx, $sPassword, $sTel, $authCd, $vipPlanCd);
 	public function getLanguage();
 	public function sendRegisterSms($sUserCd, $stel);
