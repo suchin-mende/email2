d895fd0088796831573fdfbb03864138e52fdf8e
Suchin <suchin@seaos.co.jp>
2021-02-17 10:24:21 +0900

    address book 追加

--- app/libraries/RainLoop/Config/Application.php
+++ app/libraries/RainLoop/Config/Application.php
@@ -181,13 +181,13 @@ class Application extends \RainLoop\Config\AbstractConfig
 			),
 
 			'contacts' => array(
-				'enable'			=> array(false, 'Enable contacts'),
+				'enable'			=> array(true, 'Enable contacts'),
 				'allow_sync'		=> array(false),
 				'sync_interval'		=> array(20),
 				'type'				=> array('sqlite', ''),
-				'pdo_dsn'			=> array('mysql:host=127.0.0.1;port=3306;dbname=rainloop', ''),
+				'pdo_dsn'			=> array('mysql:host=127.0.0.1;port=3306;dbname=mongolEmail', ''),
 				'pdo_user'			=> array('root', ''),
-				'pdo_password'		=> array('', ''),
+				'pdo_password'		=> array('root', ''),
 				'suggestions_limit' => array(30)
 			),
 
